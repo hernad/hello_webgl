84a76648c7423e65e8cc529238b057cbe8744d52
Ernad Husremovic <hernad@bring.out.ba>
2012-08-22 17:38:01 +0200

    index.html

--- options.js
+++ options.js
@@ -15,10 +15,13 @@ function save_options() {
   localStorage["favorite_color"] = color;
   // Update status to let user know options were saved.
   var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
+  status.innerHTML = "Opcija snimljena. Idem na glavnu stranicu ...";
   setTimeout(function() {
     status.innerHTML = "";
+    window.location.href = 'index.html';
   }, 750);
+  
+  
 }
 // Restores select box state to saved value from localStorage.
 function restore_options() {
